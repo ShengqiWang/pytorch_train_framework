8b958b0294ee975548262e341a094c3133ef2fe2
CatW <46523891+ShqWW@users.noreply.github.com>
2023-05-02 01:18:15 +0800

    Update train_parse.py
    update

--- train_parse.py
+++ train_parse.py
@@ -3,17 +3,17 @@ import argparse
 def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='train-config')
-    
+    #device setting
     parser.add_argument('--is_multigpu', default=1, type=bool)
     parser.add_argument('--is_gpu', default=1, type=bool)
     parser.add_argument('--gpu_no', default=0, type=int)
-    
+    #loss recorder setting
     parser.add_argument('--use_tensorboard', default=True, type=bool)
-    
+    parser.add_argument('--iter_display', default=2, type=int)
+    #training setting
     parser.add_argument('--batch_size', default=1000, type=int)
     parser.add_argument('--epoch_num', default=10, type=int)
     parser.add_argument('--lr', default=0.01, type=int)
-    parser.add_argument('--iter_display', default=2, type=int)
     parser.add_argument('--lr_milestone', default=[3, 8], type=list)
 
     args = parser.parse_args()
